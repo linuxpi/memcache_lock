4142782a23ce5f6e575af628a9c8e3bd2b9ca162
linuxpi <varunb94@gmail.com>
2018-12-27 15:01:28 +0530

    add description for pypi

--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
-from setuptools import setup
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name='memcache_lock',
-    version='0.1',
+    version='0.0.3',
     description='Locking mechanism for Python 2.7 GAE, using memcache',
+    long_description=read('README.md'),
     url='https://github.com/linuxpi/memcache_lock',
     author='Varun Bansal',
     author_email='varunb94@gmail.com',
     license='MIT',
-    packages=['memcache_lock'],
-    zip_safe=False
+    packages=find_packages(),
+    zip_safe=False,
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2'
+    )
 )
